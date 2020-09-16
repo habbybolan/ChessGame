eb1a3ecabd44e5c850b81cbf7f08cd2937670263
Nicholas Johnson <habbybolan@hotmail.com>
2020-09-16 15:13:54 -0700

    fixed bug where king in threat would move to illegal spot  - removed code to specifically move king, now relies of fitness level to move king fixed bug where the best col and row of a black piece to move would not update, defaulting the best move piece to (0,0)

--- ChessGame/moveBlackPiece.c
+++ ChessGame/moveBlackPiece.c
@@ -13,59 +13,51 @@
 
 int rowBest;
 int colBest;
+struct chessPiece* bestCp;
 
 // moves the fittest piece to (rowBest, rowCol) tile
 	// if no fittest, then random move
 void moveHighestFitness() {
-	struct chessPiece* cp = bestPieceToMove();
+	rowBest = -1;
+	colBest = -1;
+	bestCp = bestPieceToMove();
 	struct chessPiece* cpW;
 
-	// checks if black king is threated
-		// if it is, move that piece to another valid spot not threatened
-			// if no spot exists, end the game
-		// otherwise move another piece
-	if (is_BlackKingThreatened()) {
-		struct chessPiece* cp = &chessPiecesBlack[14];
-		// check if there is a place to move the king that has no threat
-		if (!movePieceNoThreat(cp)) { 
-			cp->is_live = 0; // if not safe places exist, set black king as defeated, ending the game
-		}
+	// if there is no best piece/move, move a piece randomly
+	if (bestCp == NULL) {
+		randomBlackMove();
 	}
+	// othewise, move the best piece somewhere, depending on the pieces it can take and protect
 	else {
-		// if there is no best piece/move, move a piece randomly
-		if (cp == NULL) {
-			randomBlackMove();
+		// if the piece is only threatened, but cant take or protect others
+			// move it to a new random location !!! make a function to move it to a safe location if one exists
+		if (bestCp->protects == 0 && bestCp->enemiesCanDefeat == 0) {
+			if (!moveBlackIfValid(bestCp)) {
+				randomBlackMove();
+			}
 		}
-		// othewise, move the best piece somewhere, depending on the pieces it can take and protect
 		else {
-			// if the piece is only threatened, but cant take or protect others
-				// move it to a new random location !!! make a function to move it to a safe location if one exists
-			if (cp->protects == 0 && cp->enemiesCanDefeat == 0) {
-				if (!moveBlackIfValid(cp)) {
-					randomBlackMove();
-				}
+			// move the best piece to (rowBest, colBest)
+			cpW = getPiece(rowBest, colBest);
+			if (cpW != NULL) {
+				cpW->row = 100;
+				cpW->col = 100;
+				cpW->is_live = 0;
 			}
-			else {
-				// move the best piece to (rowBest, colBest)
-				cpW = getPiece(rowBest, colBest);
-				if (cpW != NULL) {
-					cpW->row = 100;
-					cpW->col = 100;
-					cpW->is_live = 0;
-				}
-				cp->row = rowBest;
-				cp->col = colBest;
-			}
+			bestCp->row = rowBest;
+			bestCp->col = colBest;
 		}
 	}
+
 }
 
 
 // find the piece with the best fittest rating, thus making it the best to move
-struct chessPiece *bestPieceToMove() {
+struct chessPiece* bestPieceToMove() {
 	setFitnessRating();
 	struct chessPiece* cpTest;
-	for (int j = 0; j < 16; j++) { //****************************************************
+	// go thorugh each piece
+	for (int j = 0; j < 16; j++) {
 		cpTest = &chessPiecesBlack[j];
 		printf("\n");
 		printf("%s has fitness: %d", cpTest->type, cpTest->fitness);
@@ -91,7 +83,7 @@ void setFitnessRating() {
 	int tempFitness; // keeps track of the highest fitness so far
 	int j; // keeps track of the iteration through listEnemiesToDefeat
 	int k; // keeps track of the itreation through listProtects
-	struct chessPiece *cp; // black chess piece to evaluate
+	struct chessPiece* cp; // black chess piece to evaluate
 	for (int i = 0; i < 16; i++) {
 		highestValPieceToTake = 0; // reset for next cp
 		tempProtect = 0; // reset for next cp
@@ -113,7 +105,7 @@ void setFitnessRating() {
 			tempProtect = 0;
 			k = 0;
 			// sets tempProtect to the most valuable piece that cp is protecting
-			while(cp->listProtects[k] != NULL && k < 16) {
+			while (cp->listProtects[k] != NULL && k < 16) {
 				if (is_validMoveWhite(cp->listProtectsRow[k], cp->listProtectsCol[k], *cp->listEnemiesCanDefeat[j])) {
 					if (getValueOfPiece(cp->listProtects[k]->type) > tempProtect) {
 						tempProtect = getValueOfPiece(cp->listProtects[k]->type);
@@ -123,12 +115,12 @@ void setFitnessRating() {
 			}
 			// makes highestValPieceToTake the largest value based on:
 				// the most valuable piece it can take, most valuable teammate it's protecting, minus its value if it is put into threat
-	
-			if ((getValueOfPiece(cp->listEnemiesCanDefeat[j]->type) - 
-				newThreat(cp, cp->listEnemiesCanDefeat[j]->row, cp->listEnemiesCanDefeat[j]->col, cp->listEnemiesCanDefeat[j]) + 
-				tempProtect) > highestValPieceToTake) {
-				highestValPieceToTake = (getValueOfPiece(cp->listEnemiesCanDefeat[j]->type) - 
-					newThreat(cp, cp->listEnemiesCanDefeat[j]->row, cp->listEnemiesCanDefeat[j]->col, cp->listEnemiesCanDefeat[j]) + 
+
+			if ((getValueOfPiece(cp->listEnemiesCanDefeat[j]->type) -
+				newThreat(cp, cp->listEnemiesCanDefeat[j]->row, cp->listEnemiesCanDefeat[j]->col, cp->listEnemiesCanDefeat[j]) +
+				tempProtect) >= highestValPieceToTake) {
+				highestValPieceToTake = (getValueOfPiece(cp->listEnemiesCanDefeat[j]->type) -
+					newThreat(cp, cp->listEnemiesCanDefeat[j]->row, cp->listEnemiesCanDefeat[j]->col, cp->listEnemiesCanDefeat[j]) +
 					tempProtect);
 				rowBest = cp->listEnemiesCanDefeat[j]->row;
 				colBest = cp->listEnemiesCanDefeat[j]->col;
@@ -136,7 +128,7 @@ void setFitnessRating() {
 			j++;
 		}
 		tempFitness = highestValPieceToTake;
-		
+
 		// set the value based on the curent threat, enemies cp can defeat, teammates it can protect and new threats after moving
 		cp->fitness = cp->fitness + tempFitness;
 
@@ -145,7 +137,7 @@ void setFitnessRating() {
 
 // ai picks a random piece and chooses a random tile to land on
 void randomBlackMove() {
-	struct chessPiece *cp;
+	struct chessPiece* cp;
 	int random = rand() % 16; // chooses a random number between 0-15
 	cp = &chessPiecesBlack[random]; // cp is the random chesspiece chosen
 	int end = 1; // allows to leave while loop once valid piece is found
@@ -154,7 +146,7 @@ void randomBlackMove() {
 	while (end) {
 		//printf("%d\n", random);
 		if (cp->is_live == 1 && cp->team == 'B' && moveBlackIfValid(cp)) {
-				end = 0;
+			end = 0;
 		}
 		else {
 			random = rand() % 16; // chooses a random number between 0-15
@@ -166,35 +158,35 @@ void randomBlackMove() {
 // helper function for randonBlackMoves
 	// choosen a random tile to place the random piece already chosen
 	// if no valid move, then return 0 and don't move piece
-int moveBlackIfValid(struct chessPiece *cp) {
-		// iterate through random moves until it finds a valid move - set end to 0
-				 // if it cant find a valid move after 20 tries, try another piece
-	struct chessPiece *tempPiece; // piece on the spot ai is moving black piece
+int moveBlackIfValid(struct chessPiece* cp) {
+	// iterate through random moves until it finds a valid move - set end to 0
+			 // if it cant find a valid move after 20 tries, try another piece
+	struct chessPiece* tempPiece; // piece on the spot ai is moving black piece
 
-		for (int i = 0; i < 50; i++) {
-				int col = rand() % 8; // chooses a random number between 0-7
-				int row = rand() % 8; // chooses a random number between 0-7
-				tempPiece = getPiece(row, col);
+	for (int i = 0; i < 50; i++) {
+		int col = rand() % 8; // chooses a random number between 0-7
+		int row = rand() % 8; // chooses a random number between 0-7
+		tempPiece = getPiece(row, col);
 
-					// checking if the move with cp piece is valid
-					if (is_validMoveBlack(row, col, *cp) && noTeamPiece(row, col, *cp)) { // move Black piece 
-						// check if there is an enemy piece at spot
-						// remove enemy piece if it is at spot to move
-						if (tempPiece != NULL) {
-							if (!noEnemyPiece(row, col, *cp)) { // then there is an enemy at the position
-								tempPiece->is_live = 0;
-								// move into invalid area - off the board
-								tempPiece->row = 100;
-								tempPiece->col = 100;
-							}
-						}
-						// move the piece - valid move
-						cp->row = row;
-						cp->col = col;
-						return 1;
-					}
+		// checking if the move with cp piece is valid
+		if (is_validMoveBlack(row, col, *cp) && noTeamPiece(row, col, *cp)) { // move Black piece 
+			// check if there is an enemy piece at spot
+			// remove enemy piece if it is at spot to move
+			if (tempPiece != NULL) {
+				if (!noEnemyPiece(row, col, *cp)) { // then there is an enemy at the position
+					tempPiece->is_live = 0;
+					// move into invalid area - off the board
+					tempPiece->row = 100;
+					tempPiece->col = 100;
+				}
+			}
+			// move the piece - valid move
+			cp->row = row;
+			cp->col = col;
+			return 1;
 		}
-		return 0;
+	}
+	return 0;
 }
 
 // gets the value of the chessPiece
@@ -202,7 +194,7 @@ int getValueOfPiece(char* type) {
 	// value of king = 1000
 	if (strcmp("WKg", type) == 0 || strcmp("BKg", type) == 0) {
 		return 1000;
-	} 
+	}
 	// value of queen = 9
 	else if (strcmp("WQn", type) == 0 || strcmp("BQn", type) == 0) {
 		return 9;
@@ -266,7 +258,7 @@ void resetFitness() {
 // find a place on the board that is safe to move to
 	// return true if a safe spot exists and move the piece there
 	// otherwise, return false, no safe spot exists
-bool movePieceNoThreat(struct chessPiece *cp) {
+bool movePieceNoThreat(struct chessPiece* cp) {
 	struct chessPiece* cpW;
 	// iterates through all pieces on the board
 	for (int i = 0; i < 8; i++) {
@@ -274,7 +266,7 @@ bool movePieceNoThreat(struct chessPiece *cp) {
 			// if row i and col j is a valid move
 			if (is_validMoveBlack(i, j, *cp)) {
 				//if no new threats at row i and col j, break loop
-				if (newThreat(cp, i, j, NULL) != 0) {
+				if (!newThreat(cp, i, j, NULL) != 0) {
 					cpW = getPiece(i, j);
 					if (cpW != NULL) {
 						cpW->row = 100;
